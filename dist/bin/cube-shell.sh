5fd8959d9779d5e572a33be7a0489c88111505d3
Patrick Stricker <github@pa2.de>
2018-01-06 20:02:59 +0000

    initial commit from pi, not working but one has to begin ;-)

---
+++ dist/bin/cube-shell.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PARENT_DIR=$( dirname ${DIR}.. )
+CONF_DIR="${PARENT_DIR}/conf"
+LIBS_DIR="${PARENT_DIR}/libs"
+
+SYSTEM_PROPERTIES="-Dapplication.home=${PARENT_DIR}"
+#SYSTEM_PROPERTIES="${SYSTEM_PROPERTIES} -Dfoo=bar"
+
+# calculate class path
+LIBS=$(find ${LIBS_DIR})
+CLASSPATH="${CONF_DIR}";
+for LIB in $(echo ${LIBS}); do
+	if [ -f ${LIB} ]; then
+		CLASSPATH="${CLASSPATH}:${LIB}"
+	fi
+done
+
+MAINCLASS="de.pa2.projects.audio.cube.CommandLineInterface"
+JAVA="java"
+
+COMMAND="${JAVA} -cp ${CLASSPATH} ${SYSTEM_PROPERTIES} ${MAINCLASS}"
+cd ${PARENT_DIR}
+echo "executing: '${COMMAND}'"
+${COMMAND}
